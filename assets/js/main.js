623490db4c03f06168b88fed72a08798f4010597
Oussama Boukhatem <oussamaboukhatem05@gmail.com>
2025-02-13 12:41:22 -0500

    Worked on product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,16 @@ const submitBtn = document.getElementById("submitBtn");
 const nameField = document.getElementById("nameField");
 
 toggleForm.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (submitBtn.textContent === "Login") {
-        formTitle.textContent = "Sign Up";
-        submitBtn.textContent = "Sign Up";
-        toggleForm.textContent = "Already have an account? Login";
-        nameField.classList.remove("d-none");
-    } else {
-        formTitle.textContent = "Login";
-        submitBtn.textContent = "Login";
-        toggleForm.textContent = "Don't have an account? Sign up";
-        nameField.classList.add("d-none");
-    }
+  e.preventDefault();
+  if (submitBtn.textContent === "Login") {
+    formTitle.textContent = "Sign Up";
+    submitBtn.textContent = "Sign Up";
+    toggleForm.textContent = "Already have an account? Login";
+    nameField.classList.remove("d-none");
+  } else {
+    formTitle.textContent = "Login";
+    submitBtn.textContent = "Login";
+    toggleForm.textContent = "Don't have an account? Sign up";
+    nameField.classList.add("d-none");
+  }
 });
